6e030f08f5e861789f0c7b2e62448cd8eb8e7416
Sergey Sharapov <ejilur@gmail.com>
2016-08-02 16:33:14 +0300

    Fixed seeds files, added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,145 +9,137 @@ Page.destroy_all
 Offer.destroy_all
 Advantage.destroy_all
 Feedback.destroy_all
+p "Cleared database before loading seeds data"
 
-Page.create!([{
-                name: "Интернет магазин женских часов/украшений",
-                title: "РАСПРОДАЖА ИНТЕРНЕТ МАГАЗИНА ЖЕНСКИХ ЧАСОВ/УКРАШЕНИЙ",
-                offer: "Распродажа  -70%",
-                footer_text: "Политика конфиденциальности"
-               }])
+
+pages_list=[
+    [
+    "Интернет магазин женских часов/украшений",
+      "РАСПРОДАЖА ИНТЕРНЕТ МАГАЗИНА ЖЕНСКИХ ЧАСОВ/УКРАШЕНИЙ",
+     "Распродажа  -70%",
+     "Политика конфиденциальности "
+    ]
+]
+offers_list=[
+    [
+        "Браслет Pandora",
+        "-50%",
+        "5635р.",
+        "1690р.",
+        "Перейти в корзину"
+    ],
+    [
+        "Браслет Swarovski",
+        "-50%",
+        "3180р.",
+        "1590р.",
+        "Перейти в корзину"
+    ],
+    [
+        "Cерьги Dior пуссеты",
+        "-50%",
+        "3720р.",
+        "1750р.",
+        "Перейти в корзину"
+    ]
+]
+
+feedbacks_list=[
+    [
+        "«Заказала по скидке браслет пандора, очень понравилось общение с оператором и быстрая доставка! Рекомендую! Когда еще будут скидки?»",
+        "Ирина Чукалина, г. Тюмень"
+    ],
+    [
+        "«Заказал жене украшение Swarovski. Теперь не расстаётся с ним)) Спасибо огромное вашей компании за предоставленную возможность!»",
+        "Максим Зюзин, г. Москва"
+    ],
+    [
+        "«Были опасения по качеству товара, в связи с такими огромными скидками, но всё оказалось в порядке, буду тут и дальше покупать, спасибо вам!)»",
+        "Мария Краснова г. Ижевск"
+    ]
+]
+advantages_list=[
+    [
+        "Доставка почтой, бандеролями 1‑го класса, в течение 5‑10 рабочих дней. Стоимость доставки товара от 350 р."
+    ],
+    [
+        "Никаких предоплат! Оплата заказов осуществляется непосредственно по факту получения заказа."
+    ],
+    [
+        "Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины возврата."
+    ]
+]
+
+
+  pages_list.each do |name, title, offer, footer_text|
+    Page.create( name: name,title: title, offer: offer, footer_text: footer_text)
+  end
+  offers_list.each do |name, discount, price_old, price, button_text|
+    Offer.create( name: name,discount: discount, price_old: price_old, price: price, button_text: button_text, page_id: 1)
+  end
+  feedbacks_list.each do |description, author|
+    Feedback.create( description: description, author: author, page_id: 1)
+  end
+  advantages_list.each do |description|
+    Advantage.create( description: description, page_id: 1)
+  end
 
 p "Created #{Page.count} pages"
-
-
-Offer.create!([
-                  {
-                   name: "Браслет Pandora",
-                   discount:"-50%",
-                   price: "5635р",
-                   price_old: "1690р",
-                   button_text: "Перейти в корзину",
-                   page_id: 1
-                  },
-                  {
-                      name: "Браслет Swarovski",
-                      discount:"-50%",
-                      price: "3180",
-                      price_old: "1590",
-                      button_text: "Перейти в корзину",
-                      page_id: 1
-                  },
-                  {
-                      name: "Cерьги Dior пуссеты",
-                      discount:"-50%",
-                      price: "3720р",
-                      price_old: "1750р",
-                      button_text: "Перейти в корзину",
-                      page_id: 1
-                  }
-              ]
-)
-
 p "Created #{Offer.count} offers"
-
-Advantage.create!([{
-                        description: "«Заказала по скидке браслет пандора, очень понравилось общение с оператором и быстрая доставка! Рекомендую! Когда еще будут скидки?»",
-                        author:"Ирина Чукалина, г. Тюмень"
-                  },
-                   {
-                       description: "«Заказал жене украшение Swarovski. Теперь не расстаётся с ним)) Спасибо огромное вашей компании за предоставленную возможность!»",
-                       author: "Максим Зюзин, г. Москва"
-                   },
-                   {
-                       description: "«Были опасения по качеству товара, в связи с такими огромными скидками, но всё оказалось в порядке, буду тут и дальше покупать, спасибо вам!)»",
-                       author: "Мария Краснова г. Ижевск"
-                   }
-                  ])
-
-p "Created #{Advantage.count} advantages blocks"
+p "Created #{Advantage.count} advantage blocks"
+p "Created #{Feedback.count} feedback blocks"
 
 
-# pages_list=[
+# Different style of seeds.rb
+# Page.create(
 #     [
-#     "Интернет магазин женских часов/украшений",
-#       "РАСПРОДАЖА ИНТЕРНЕТ МАГАЗИНА ЖЕНСКИХ ЧАСОВ/УКРАШЕНИЙ",
-#      "Распродажа  -70%",
-#      "Политика конфиденциальности "
+#                  {
+#                 name: "Интернет магазин женских часов/украшений",
+#                 title: "РАСПРОДАЖА ИНТЕРНЕТ МАГАЗИНА ЖЕНСКИХ ЧАСОВ/УКРАШЕНИЙ",
+#                 offer: "Распродажа  -70%",
+#                 footer_text: "Политика конфиденциальности"
+#                }
 #     ]
-# ]
-# offers_list=[
-#     [
-#         "Браслет Pandora",
-#         "-50%",
-#         "5635р.",
-#         "1690р.",
-#         "Перейти в корзину"
-#     ],
-#     [
-#         name:"Браслет Swarovski",
-#         "-50%",
-#         "3180р.",
-#         "1590р.",
-#         "Перейти в корзину"
-#     ],
-#     [
-#         "Cерьги Dior пуссеты",
-#         "-50%",
-#         "3720р.",
-#         "1750р.",
-#         "Перейти в корзину"
-#     ]
-# ]
-#
-# feedbacks_list=[
-#     [
-#         "«Заказала по скидке браслет пандора, очень понравилось общение с оператором и быстрая доставка! Рекомендую! Когда еще будут скидки?»",
-#         "Ирина Чукалина, г. Тюмень"
-#     ],
-#     [
-#         "«Заказал жене украшение Swarovski. Теперь не расстаётся с ним)) Спасибо огромное вашей компании за предоставленную возможность!»",
-#         "Максим Зюзин, г. Москва"
-#     ],
-#     [
-#         "«Были опасения по качеству товара, в связи с такими огромными скидками, но всё оказалось в порядке, буду тут и дальше покупать, спасибо вам!)»",
-#         "Мария Краснова г. Ижевск"
-#     ]
-# ]
-# advantages_list=[
-#     [
-#         "Доставка почтой, бандеролями 1‑го класса, в течение 5‑10 рабочих дней. Стоимость доставки товара от 350 р.",
-#     "Никаких предоплат! Оплата заказов осуществляется непосредственно по факту получения заказа.",
-#         "Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины возврата."
-#     ]
-# ]
-#
-#
-#   pages_list.each do |name, title, offer, footer_text|
-#     Page.create( name: name,title: title, offer: offer, footer_text: footer_text)
-#   end
-#   offers_list.each do |name, discount, price_old, price, button_text|
-#     Offer.create( name: name,discount: discount, price_old: price_old, price: price, button_text: button_text, page_id: 1)
-#   end
-#   feedbacks_list.each do |description, author|
-#     Feedback.create( description: description, author: author, page_id: 1)
-#   end
-#   advantages_list.each do |description|
-#     Advantage.create( description: description, page_id: 1)
-#   end
+# )
 
+# Offer.create([
+#                   {
+#                    name: "Браслет Pandora",
+#                    discount:"-50%",
+#                    price: "5635р",
+#                    price_old: "1690р",
+#                    button_text: "Перейти в корзину",
+#                    page_id: 1
+#                   },
+#                   {
+#                       name: "Браслет Swarovski",
+#                       discount:"-50%",
+#                       price: "3180",
+#                       price_old: "1590",
+#                       button_text: "Перейти в корзину",
+#                       page_id: 1
+#                   },
+#                   {
+#                       name: "Cерьги Dior пуссеты",
+#                       discount:"-50%",
+#                       price: "3720р",
+#                       price_old: "1750р",
+#                       button_text: "Перейти в корзину",
+#                       page_id: 1
+#                   }
+#               ]
+# )
 
-
-# posts_list = [
-#     [ "1_Great Post", "*Post1* Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-# tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-# quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" ],
-#     [ "2_Awesome event", "*Post2* consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-# cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-# proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ],
-#     [ "3_New knowledge", "*Post3* Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-# tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-# quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo" ],
-#     [ "4_Clear Vision", "*Post4* Duis aute irure dolor in reprehenderit in voluptate velit esse
-# cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-# proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ]
-# ]
+# Advantage.create!([{
+#                         description: "«Заказала по скидке браслет пандора, очень понравилось общение с оператором и быстрая доставка! Рекомендую! Когда еще будут скидки?»",
+#                         author:"Ирина Чукалина, г. Тюмень"
+#                   },
+#                    {
+#                        description: "«Заказал жене украшение Swarovski. Теперь не расстаётся с ним)) Спасибо огромное вашей компании за предоставленную возможность!»",
+#                        author: "Максим Зюзин, г. Москва"
+#                    },
+#                    {
+#                        description: "«Были опасения по качеству товара, в связи с такими огромными скидками, но всё оказалось в порядке, буду тут и дальше покупать, спасибо вам!)»",
+#                        author: "Мария Краснова г. Ижевск"
+#                    }
+#                   ])
