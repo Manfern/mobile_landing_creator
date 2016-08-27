b543f1807bae31f38e3bd074822374762852e9f9
Sergey Sharapov <ejilur@gmail.com>
2016-08-27 21:50:45 +0300

    Used Ryan Bates cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,24 @@ set :keep_releases, 3
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
   namespace :deploy do
+    # config for unicorn and nginx
+    %w[start stop restart].each do |command|
+      desc "#{command} unicorn server"
+      task command, roles: :app, except: {no_release: true} do
+        run "/etc/init.d/unicorn_#{application} #{command}"
+      end
+    end
 
+    task :setup_config, roles: :app do
+      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+      run "mkdir -p #{shared_path}/config"
+      put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+      puts "Now edit the config files in #{shared_path}."
+    end
+
+    after "deploy:setup", "deploy:setup_config"
+    # end of config for unicorn and nginx
     desc 'Restart application'
     task :restart do
       on roles(:app), in: :sequence, wait: 5 do
