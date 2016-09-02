ade98fd6a6aece922efaf03eebbc585d862a13ee
Sergey Sharapov <ejilur@gmail.com>
2016-09-02 15:44:46 +0300

    fixed login page

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,10 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
+  # devise :database_authenticatable, :registerable,
+         # :recoverable, :rememberable, :trackable, :validatable
   has_many :pages
   attr_accessor :selected_page
 end
