e0db9a439715ffa1d50ef7d7a26a6c86e16676d5
Sergey Sharapov <ejilur@gmail.com>
2016-09-04 06:43:27 +0300

    change dark theme colors

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,21 +6,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   # include ::CarrierWave::Backgrounder::Delay
   # include CarrierWave::MimeTypes
-  # process :set_content_type
-  # Include RMagick or MiniMagick support:
 
-  # include CarrierWave::MiniMagick
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+
+
+
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :file
+
   storage :fog
 
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
 
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
